238ebf47bc391219d9648902163c3a40e831e31e
Hayden Kelley <haydenkelley11@gmail.com>
2023-03-11 16:21:41 -0600

    Add files via upload

---
+++ week 03 updated final.js
@@ -0,0 +1,94 @@
+int[] ages = {3, 9, 23, 64, 2, 8, 28};
+
+int lengthOfArr = ages.length;
+
+int difference =  Math.abs(ages[lengthOfArr - lengthOfArr] - ages[lengthOfArr - 1]);
+
+System.out.printIn("Difference of first and last element in ages[] = "+ difference);
+
+int[]newAges = new int [ages.length];
+
+/* 2
+
+String[] names = {"Sam", "Tommy", "Tim" , "Sally", "Buck", "Bob"};
+sum=0
+for (int i = 0; i < names.length; i++) {
+    sum += names[i].length();
+
+}
+average = sum / names.length;
+Systemout.printIn("The average number of letters per name in names[] = " + average );
+
+String allNames = "";
+for (int i = 0; i < names.length; i++) {
+
+}
+System.out.printIn(allNames);
+
+/*3 */
+
+System.out.printIn("Use array[arr.length] to access the last element of an array");
+
+*/4 */
+
+System.out.printIn("Use array[0] to access the first element of an array");
+
+*/ 5 */
+
+int[] nameLengths = new int[names.Length];
+for (int i = 0; i < names.lenth; i++) {
+    nameLengths[i] = names [i].length();
+
+}
+
+/* 6 */
+
+sum = 0;
+for (int i = 0; i < nameLength.length; i++) {
+    sum += nameLengths[i];
+
+}
+System.out.printIn("The sum of all lengths in nameLengths[] =" + sum);
+
+  /* 7 */
+
+  System.out.printIn(duplicateWord("Hello", 5));
+
+  */8 */
+
+  System.out.printIn(getFullName("Hayden". "Kelley"));
+
+  */9
+
+  System.out.printIn("checkIfSumGreaterYhan100() returns => " + checkIfSumGreaterThan100(ages));
+
+  */10/*
+
+  double[] doubleArr = {1.1, 2.2, 3.3, 4.4,};
+  double[] emptyDoubleArr = {};
+  System.out.print.In(" getAverageOfDoubleArr() returns => " + getAverageOfDoubleArr(doubleArr));
+  System.out.printIn("Passing empty double[] to getAverageOfDoubleArr() returns => " + getAverageOfDoubleArr));
+
+  /* 11 */
+
+  double[] anotherDoubleArr = {1, 2, 3, 4};
+  System.out.printIn("isFirstArrAvgGreater returns => " + isFirstArrAvgGreater(doubleArr, anotherDoubleArr));
+
+  /* 12 
+
+  function willBuyDrink(isHotOutside) {
+    if (moneyInPocket < 10.50){
+        return "true";
+    }else{
+        return "true";
+    }
+  }
+
+  console.log(willBuyDrink(10)); //"false"
+  console.log(willBuyDrink(10.50)) //"true"
+
+
+
+
+
+
